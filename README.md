68a0a464dbf5e63985a904eba76bfb5ce1edd382
Snorre Magnus Davøen <snorre.magnus.davoen@adventuretech.no>
2024-09-23 15:28:44 +0200

    feat: CSV log of process terminations and improvements

--- README.md
+++ README.md
@@ -22,9 +22,38 @@ See [./example.toml](./example.toml) file for a config example.
 To get correct CPU usage you need to run the program with the correct privileges.
 E.g. for processes owned by root use sudo to run process as root.
 
+
+### Add as a MacOS service
+
+If you don't want to install with Homebrew, first clone the project and build the binary:
+
+```sh
+git clone git@github.com:snorremd/process-bonk.git
+cd process-bonk
+cargo run build --release
+```
+
+Then add the binary as a service.
+Replace `<path-to-binary>` and `<path-to-config>` in the `.plist` file with the correct paths to the binary and config file.
+
+```sh
+cp example.toml process-bonk.toml # make a copy of example config
+cp example.plist process-bonk.plist # make a copy of example plist
+open process-bonk.toml # Edit the config file to specify correct process to monitor
+open process-bonk.plist # Edit the plist file to point to the correct binary path
+sudo chown root:wheel process-bonk.plist
+sudo chmod 644 process-bonk.plist
+sudo cp process-bonk.plist /Library/LaunchDaemons/io.snorre.process-bonk.plist
+sudo launchctl load /Library/LaunchDaemons/io.snorre.process-bonk.plist
+```
+
+You are free to put the binary and config files wherever you want.
+The plist file should point to the correct binary and config paths.
+
 ### Homebrew service
 
-If you want to run it as a service:
+Running the service with Homebrew as root is not recommended.
+You can run as non-root user though:
 
 ```sh
 # Specify config
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use std::fs::OpenOptions;
+use std::io::Write;
 use std::{env, fs};
 use std::time::{Duration, Instant};
 use std::thread::sleep;
-use sysinfo::{System, Signal};
+use sysinfo::{ProcessesToUpdate, Signal, System};
 use toml::Value;
-use native_dialog::{MessageDialog, MessageType};
+use std::ffi::OsStr;
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let mut system = System::new_all();
     let mut high_cpu_usage_start: Option<Instant> = None;
     let mut pid = None;
@@ -34,44 +36,59 @@ fn main() {
         .and_then(Value::as_str)
         .expect("Could not find 'process_name' in config file");
 
+    let os_process_name: &OsStr = OsStr::new(process_name);
+
+    let stats_path = config.get("stats_path")
+        .and_then(Value::as_str)
+        .expect("Could not find 'stats_path' in config file");
+
+    let mut csv_file = OpenOptions::new()
+        .append(true) // Open in append mode
+        .create(true) // Create the file if it doesn't exist
+        .open(stats_path)?;
+
+    println!("Length of file, {}", csv_file.metadata()?.len());
+
+    // Check if stats is empty, if so add headers
+    if csv_file.metadata()?.len() == 0
+    {
+        writeln!(csv_file, "date time, process_name")?;
+    }
+    
     // Log that we are monitoring the process
     println!("process-bonk is now monitoring the {} process", process_name);
 
     loop {
         // Set pid if it is not set yet
-        if pid.is_none() {
-            if let Some(process) = system.processes_by_exact_name(process_name).next() {
+        if pid.is_none() || !system.process(pid.unwrap()).is_some() {
+            if let Some(process) = system.processes_by_exact_name(os_process_name).next() {
                 pid = Some(process.pid());
                 println!("Found {} process with PID {}", process_name, pid.unwrap());
             }
         }
 
         // Update the specific process information in sysinfo
-        system.refresh_process(pid.unwrap());
+        system.refresh_processes(ProcessesToUpdate::Some(&[pid.unwrap()]));
         
         if let Some(process) = system.process(pid.unwrap()) {
 
             let cpu_usage = process.cpu_usage();
 
-            if cpu_usage >= 98.0 {
+            if cpu_usage >= 90.0 {
                 match high_cpu_usage_start {
-                    Some(start_time) if start_time.elapsed() > Duration::from_secs(300) => {
+                    Some(start_time) if start_time.elapsed() > Duration::from_secs(60*15) => {
                         // If the process has been using 100% CPU for more than 5 minutes, kill it
-                        println!("{} has been using lots of CPU for more than 5 minutes, bonking it", process_name);
-                        let yes = MessageDialog::new()
-                            .set_type(MessageType::Info)
-                            .set_title("Process Bonk")
-                            .set_text(&format!("{} is misbehaving, kill it?", process_name))
-                            .show_confirm()
-                            .unwrap();
-                        if yes {
-                            println!("User chose to kill {}", process_name);
-                            process.kill_with(Signal::Kill);
-                        } else {
-                            println!("User chose not to kill {}", process_name);
-                        }
+                        println!("{} has been using lots of CPU for more than 2 minutes, bonking it", process_name);
+                        process.kill_with(Signal::Kill);
+
+                        // Append to stats_path CSV file that we bonked the process in format: date time, process_name. Use standard library
+                        let row = format!("{}, {}", chrono::Local::now().format("%Y-%m-%d %H:%M:%S"), process_name);
+                        // This line does not append to the file, it overwrites it. Use append instead of write
+                        writeln!(csv_file, "{}", row)?;
+                        
                         // User has been notified, reset the tracking
                         high_cpu_usage_start = None;
+                        pid = None;
                     }
                     None => {
                         // Start tracking when the process started using 100% CPU
